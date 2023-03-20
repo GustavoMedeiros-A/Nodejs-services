a4b676adae0545bb3cdc6025180e2a0fdce185ae
Gustavo <gustavomedeiros15@outlook.com.br>
2023-03-20 17:58:17 -0300

    Creation of the salary function, who return the final salaty of one employer

--- src/router/personRouter.js
+++ src/router/personRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const Person = require('../model/personModel')
 
-const { calculateSalary } = require('../service/personService');
+const { calculateSalary, calculateNumberChildren} = require('../service/personService');
 
 router.get('/person/getAll', async (req, res) => {
     try {
@@ -17,8 +17,9 @@ router.get('/person/getAll', async (req, res) => {
 
 router.get('/person/:id', async (req, res) => {
     try{
-        let name = await Person.findById(req.params.id);
-        res.status(200).json(`Name of the person: ${name}`)
+        let person = await Person.findById(req.params.id);
+        // res.status(200).json(`Name of the person: ${name}`)
+        res.status(200).json({person})
     } catch (error) {
         console.log(error)
     }
@@ -45,13 +46,49 @@ router.delete('/people/:id', async (req, res) => {
     }
 });
 
-router.get('/salaries', async (req, res) => {
+// router.get('/salaries/:id', async (req, res) => {
+//     try {
+//         let person = await Person.findById(req.params.id);
+//         let childrenValue = 0
+//         let inss = 0.13
+//         let discountVoucher = 0.06
+//         let finalSalary = 0
+//         if (person.childrenNumber >= 3) {
+//             childrenValue = 150
+//         } else {
+//             childrenValue = person.childrenNumber * 50
+//         }
+//         if(person.nightWorker === true) {
+//             finalSalary = person.payCheck - (person.payCheck * inss) - (person.payCheck * discountVoucher) + (person.payCheck * 0.05) + childrenValue
+//         } else {
+//             finalSalary = person.payCheck - (person.payCheck * inss) - (person.payCheck * discountVoucher) + childrenValue
+//         }
+//         res.status(200).json(finalSalary)
+        
+//     } catch (error) {
+//         console.log(error)
+//     }
 
+// })
+
+router.get('/salaries/:id', async (req, res) => {
+    try {
+        let person = await Person.findById(req.params.id);
+        // let childrenValue = 0
+        let childrenValue = calculateNumberChildren(person);
+        console.log(childrenValue)
+        res.status(200).json(childrenValue)
+        
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+})
 
     //usar o mongoose pra pegar todos os funcionarios
     //fazer um loop por cada funcionario chamando a funcao que calcula seu salario
     //e por fim ir adicionando o valor calculado pela função na variavel que sera retornada
-    return salarioDeTodosFuncionarios;
-})
+    // return salarioDeTodosFuncionarios;
 
 module.exports = router
--- src/service/personService.js
+++ src/service/personService.js
@@ -1,4 +1,5 @@
-export const calculateSalary = async () => {
+
+const calculateSalary = async () => {
     let inss = 0.13
     let discountVoucher = 0.06
     if(nightWorker === true) {
@@ -9,8 +10,26 @@ export const calculateSalary = async () => {
     return finalSalary
 }
 
-const calculateNumberChildren = async () => {
-    return (childrenNumber >= 3) ? 150 : childrenNumber  * 50
+const calculateNumberChildren = async (person) => {
+    return (person.childrenNumber >= 3) ? 150 : person.childrenNumber  * 50
 }
+// const Person = require('../model/personModel')
 
-export default calculateNumberChildren;
+
+// const calculateNumberChildren = async (res, req) => {
+//     try {
+//         let person = await Person.findById(req.params.id);
+//         let childrenValue = 0
+//         if (person.childrenNumber >= 3) {
+//             childrenValue = 150
+//         } else {
+//             childrenValue = person.childrenNumber * 50
+//         }
+//         res.status(200).json(childrenValue)
+        
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+module.exports = { calculateSalary, calculateNumberChildren };
